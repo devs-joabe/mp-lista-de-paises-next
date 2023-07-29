66841c3b74c77557c1746b75280c57623921151c
devs-joabe <joabefnascimento1@outlook.com>
2023-07-29 00:44:30 -0300

    novo commit 2

--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -1,10 +1,10 @@
-import { Country } from "@/app/page";
+import type { Country } from "@/app/page";
 
 async function getCountryByName(name: string): Promise<Country> {
   const response = await fetch(
     `https://restcountries.com/v3.1/name/{name}?fullText=true`
   );
-  return response.json();
+  return (await response.json())[0];
 }
 
 export default async function CountryPage({
@@ -13,5 +13,6 @@ export default async function CountryPage({
   params: { name: string };
 }) {
   const country = await getCountryByName(name);
+
   return <h1>{country.name.common}</h1>;
 }
